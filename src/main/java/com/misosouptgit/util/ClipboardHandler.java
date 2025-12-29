267d4d8ab0c4c42a182fdbd7bfa568250a52ca28
misopy <misopy@outlook.com>
2025-12-29 21:55:15 +0900

    とにかく！！色んな！！変更！！

--- src/main/java/com/misosouptgit/util/ClipboardHandler.java
+++ src/main/java/com/misosouptgit/util/ClipboardHandler.java
@@ -4,10 +4,5 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 public interface ClipboardHandler {
-    /**
-     * @param image n
-     * @param file n
-     * @return n
-     */
     boolean copyToClipboard(BufferedImage image, File file);
 }
